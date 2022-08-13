9eb0f8a66d50b2131d3a493a767a23b194ce4fbf
yl <yl.code@qq.com>
2022-08-13 18:26:00 +0800

    ts-loader 编译 tsx 代码

---
+++ src/react-demo/hello-react.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return <h1>hello react !!!</h1>;
+};
+
+ReactDOM.render(<App />, document.querySelector('#root'));
---
+++ webpack.react.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    hello: './src/react-demo/hello-react.tsx',
+  },
+  output: {
+    filename: 'bundle.[name].js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '/src/index.html'),
+    }),
+  ],
+};
